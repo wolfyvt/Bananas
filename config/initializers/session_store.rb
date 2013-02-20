63fd40fe6ca6e6a01c73d1adbdd2e33016e8a8c3
Adam Wolf <adam.t.wolf@gmail.com>
2013-02-19 17:49:00 -0800

    first commit.  Untouched project

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Bananas::Application.config.session_store :cookie_store, key: '_bananas_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Bananas::Application.config.session_store :active_record_store
